029fe9baed87c551410cd8328b87b6abe013cf9b
mattdodge <matt@n.io>
2015-11-05 11:09:31 -0800

    Add alias to find unused blocks in a nio project

--- nio/aliases.zsh
+++ nio/aliases.zsh
@@ -2,3 +2,17 @@ test_block() {
 	coverage run `which nose2` $1.tests
 	coverage report --include="$1/*" -m
 }
+
+unused_blocks() {
+for blockcfg in etc/blocks/*; do 
+  block=$(echo $blockcfg | sed 's/.*\/\(.*\)\.cfg/\1/'); 
+  grep $block etc/services/* > /dev/null || echo $block; 
+done
+}
+
+remove_unused_blocks() {
+for blockcfg in etc/blocks/*; do 
+  block=$(echo $blockcfg | sed 's/.*\/\(.*\)\.cfg/\1/'); 
+  grep $block etc/services/* > /dev/null || rm -f $blockcfg; 
+done
+}
