dddc2fd4b3374dcbf9efc274b30e5d02f7423751
mattdodge <matt@n.io>
2016-02-10 14:48:46 -0800

    Stop using git plugin, add diff-so-fancy

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,10 +7,12 @@ then
 fi
 
 # The rest of my fun git aliases
+alias g='git'
+alias ga='git add'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
-alias gd='git diff'
+gd() { git diff --color $* | diff-so-fancy | less --tabs=1,5 -R }
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
