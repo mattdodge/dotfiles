e5eeb9fd88e7b9404de6977731760193f1ad97e7
mattdodge <matt@n.io>
2016-02-10 14:49:11 -0800

    Allow spaces in block names for nio unused blocks script

--- nio/aliases.zsh
+++ nio/aliases.zsh
@@ -6,13 +6,13 @@ test_block() {
 unused_blocks() {
 for blockcfg in etc/blocks/*; do 
   block=$(echo $blockcfg | sed 's/.*\/\(.*\)\.cfg/\1/'); 
-  grep $block etc/services/* > /dev/null || echo $block; 
+  grep "$block" etc/services/* > /dev/null || echo $block; 
 done
 }
 
 remove_unused_blocks() {
 for blockcfg in etc/blocks/*; do 
   block=$(echo $blockcfg | sed 's/.*\/\(.*\)\.cfg/\1/'); 
-  grep $block etc/services/* > /dev/null || rm -f $blockcfg; 
+  grep "$block" etc/services/* > /dev/null || rm -f $blockcfg; 
 done
 }
