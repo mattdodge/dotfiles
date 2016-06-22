3160af78e46c12df29a0cb7ab372c79d89bc66df
mattdodge <matt@n.io>
2016-06-22 14:59:05 -0700

    Add virtualenvwrapper env variables

---
+++ python/env.zsh
@@ -0,0 +1,5 @@
+export WORKON_HOME=$HOME/venv
+export PROJECT_HOME=$HOME/nioenv
+export VIRTUALENVWRAPPER_PYTHON=`which python3.4`
+export VIRTUALENVWRAPPER_VIRTUALENV=`which virtualenv-3.4`
+source `which virtualenvwrapper.sh`
