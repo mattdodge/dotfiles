f3a7730bb2fc8f8e958d4edb6392db650acdb6d5
mattdodge <matt@n.io>
2016-08-25 15:00:36 -0700

    Add alias to sleep computer in X minutes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -29,3 +29,11 @@ alias flush="sudo killall -HUP mDNSResponder" # Flush DNS cache
 alias gzip="gzip -9n" # set strongest compression level as ‘default’ for gzip
 alias ping="ping -c 5" # ping 5 times ‘by default’
 alias ql="qlmanage -p 2>/dev/null" # preview a file using QuickLook
+
+# Schedule sleep in X minutes, use like: sleepin 60
+function sleepin() {
+	local minutes="$1"
+	local datetime="`date -v+${minutes}M +"%m/%d/%y %H:%M:%S"`"
+	sudo pmset schedule sleep "$datetime"
+	echo "Going to sleep at $datetime"
+}
