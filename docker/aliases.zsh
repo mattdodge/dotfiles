0cfd9fa6c1420eb53c163ecbe4f86f15d9342344
mattdodge <matt@n.io>
2016-11-22 08:50:02 -0800

    Add docker shell cmd and zsh docker plugins

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -6,3 +6,6 @@ alias docker-image-clean='docker rmi $(docker images -q -f dangling=true)'
 
 # Stop, remove, build, and run current docker compose project
 alias docker-compose-cycle='docker-compose stop && docker-compose rm -f && docker-compose build && docker-compose up -d'
+
+# Enter a shell in a container
+docker-shell() { docker exec -i -t $1 /bin/bash; }
