4b1f18cc4f5813a36471281dadbc7de56e675ce5
mattdodge <matt@n.io>
2017-07-06 16:10:30 -0700

    Add some terraform aliases

--- ansible/aliases.zsh
+++ ansible/aliases.zsh
@@ -1,1 +1,5 @@
 alias ap='ansible-playbook'
+
+# An alias to run ansible-playbook with a terraform inventory
+# It uses the `terraform-inventory` script as an inventory and assumes python3 on the target hosts
+alias ap-terra='ansible-playbook -i `which terraform-inventory` -e "ansible_python_interpreter=/usr/bin/python3"'
