a902c0058e9fc17061846d63570d3a305f49efa4
mattdodge <matt@n.io>
2018-02-05 17:58:59 -0800

    helper to use virtualenv in pyenv

--- python/path.zsh
+++ python/path.zsh
@@ -1,1 +1,6 @@
 export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/2.7/bin:/Library/Frameworks/Python.framework/Versions/Current/bin
+
+# For some reason this crashes terminals when uncommented
+#eval "$(pyenv init -)"
+# Run this to allow for using mkvirtualenv
+#pyenv virtualenvwrapper
