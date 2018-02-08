57c983bc6e33b98ea0faf91692b2eafd4c3ce76a
mattdodge <matt@n.io>
2018-02-07 17:10:51 -0800

    better vim virtualenv support

--- python/env.zsh
+++ python/env.zsh
@@ -1,5 +1,6 @@
 export WORKON_HOME=$HOME/venv
 export PROJECT_HOME=$HOME/nioenv
+export PYENV_ROOT=$HOME/.pyenv
 export VIRTUALENVWRAPPER_PYTHON=`which python3.4`
 export VIRTUALENVWRAPPER_VIRTUALENV=`which virtualenv-3.4`
 source `which virtualenvwrapper.sh`
