466d28e14dee21c7dbb58835b7a0dc17130e5a96
mattdodge <matt@n.io>
2018-03-22 08:08:15 -0700

    kubectl-restart script until its provided by kubernetes

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,3 +1,11 @@
 kubectl-view-secret() {
 	kubectl get secret $1 -o json | jq '.data | map_values(@base64d)'
 }
+
+kubectl-restart() {
+	kubectl get deploy $1 -o json | jq \
+		'del(
+			 .spec.template.spec.containers[0].env[] 
+			 | select(.name == "RESTART_")) 
+		| .spec.template.spec.containers[0].env += [{name: "RESTART_", value: now|tostring}]' | kubectl apply -f -
+}
