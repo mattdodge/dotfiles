ff606e6599381f540b0fb7a38be976fea5946c94
Matt Dodge <mattedgod@gmail.com>
2019-08-27 14:36:25 -0700

    Terraform vim formatting on save

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,6 @@
 # GRC colorizes nifty unix tools all over the place
 if (( $+commands[grc] )) && (( $+commands[brew] ))
 then
+	# TODO: Remove make from the bashrc aliases - it messes up tty
   source `brew --prefix`/etc/grc.bashrc
 fi
