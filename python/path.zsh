044921b2ad7cca2bee3eed0c99ef7d57e859a059
Matt Dodge <mattedgod@gmail.com>
2021-12-09 09:09:29 -0800

    update pyenv configuration init

--- python/path.zsh
+++ python/path.zsh
@@ -1,4 +1,6 @@
 export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/2.7/bin:/Library/Frameworks/Python.framework/Versions/Current/bin
 
 # For some reason this crashes terminals when uncommented
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
