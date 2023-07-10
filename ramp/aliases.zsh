c083ec1bae80cd555b5b06e9c0751c28a6694fd9
Matt Dodge <mattedgod@gmail.com>
2023-07-10 11:07:38 -0700

    some ramp updates

--- ramp/aliases.zsh
+++ ramp/aliases.zsh
@@ -1,1 +1,12 @@
 alias sshqa="devtool dedicated-ssh-to-container --env qa --app core --session-duration 1800"
+gensg () {
+	cat <<EOF
+rules:
+  - id: new-rule
+    languages: [python]
+    severity: WARNING
+    message: foo
+    patterns:
+      - pattern: a
+EOF
+}
