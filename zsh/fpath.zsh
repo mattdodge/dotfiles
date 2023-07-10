df0df29a7792ea60827729ac4106d16267cd2300
Matt Dodge <mattedgod@gmail.com>
2023-07-10 13:23:57 -0700

    fix git completion with hub

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,5 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($ZSHDOT/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+# Include ZSH's completion functions in fpath too
+fpath=("$(brew --prefix)/share/zsh/site-functions" $fpath)
